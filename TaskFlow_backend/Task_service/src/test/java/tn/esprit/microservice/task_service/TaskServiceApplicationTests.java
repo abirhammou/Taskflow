dd6adf264e44a3fdd3f431be00320fb3a130265f
Abir Hammou <abirhammou2001@gmail.com>
2026-04-25 20:47:31 +0200

    fix: resolve Maven configuration issues

---
+++ TaskFlow_backend/Task_service/src/test/java/tn/esprit/microservice/task_service/TaskServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.microservice.task_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
