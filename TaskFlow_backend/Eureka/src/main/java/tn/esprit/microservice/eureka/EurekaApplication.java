23a5d854636902bf53b47f4dabb32b635b41ea85
Abir Hammou <abirhammou2001@gmail.com>
2026-04-26 18:24:54 +0200

    fix: configure Eureka server

--- TaskFlow_backend/Eureka/src/main/java/tn/esprit/microservice/eureka/EurekaApplication.java
+++ TaskFlow_backend/Eureka/src/main/java/tn/esprit/microservice/eureka/EurekaApplication.java
@@ -2,7 +2,10 @@ package tn.esprit.microservice.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaApplication {
 
